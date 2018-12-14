8af62a83c84868978c942bd4fad937086de91580
Your Name <you@example.com>
2018-12-14 15:13:37 +0900

    Add chat member add function

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,10 @@
 $(function(){
+  var formField = $('.chat-group-form__field')
+  var addButton = $('.chat-group-user__btn chat-group-user__btn--add')
+  var removeButton = $('.user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn')
   var searchResult = $('#user-search-result');
   var userForm = $('.chat__group-user_name');
+  var addedMembers = $('#chat-group-users')
   var preWord;
 
   function editElement(element) {
@@ -22,6 +26,15 @@ $(function(){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">"一致するユーザーはいません"</p>
                 </div>`
+  }
+
+  function appendMember(member) {
+    var html = `<div class='chat-group-user clearfix js-chat-member', id="chat-group-user-${ member.id }">
+                  <input name='group[user_ids][]' type='hidden' value='${ member.id }'>
+                  <p class='chat-group-user__name'>${ member.name }</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+    addedMembers.append(html);
   }
 
   userForm.on("keyup", function(){
@@ -74,6 +87,20 @@ $(function(){
       }
       console.log(users)
     })
+
+    $(document).on('click', '.chat-group-user__btn--add', function(){
+      var member = {};
+      member.id = $(this).attr("data-user-id");
+      member.name = $(this).attr("data-user-name");
+      $(this).parent().remove();
+      appendMember(member);
+    });
+
+
+    $(formField).on('click', 'removeButton', function(){
+    });
+
+
 
   });
 });
