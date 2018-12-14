3f0b4beecccc19d9461da32547b11ad58d49ba52
Your Name <you@example.com>
2018-12-14 13:57:51 +0900

    Fix ajax

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,30 +1,69 @@
 $(function(){
+  var searchResult = $('#user-search-result');
+  var userForm = $('.chat__group-user_name');
+  var preWord;
 
+  function editElement(element) {
+    var result = "^" + element;
+    return result;
+  }
+
+//htm
   function appendUser(user){
     var html = `<div class="chat-group-user clearfix">
                   <p class="chat-group-user__name">${user.name}</p>
                   <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
                 </div>`
+    searchResult.append(html)
   }
 
   function appendNoUser(user) {
+    var picked_name = $('')
     var html = `<div class="chat-group-user clearfix">
-                  <p class="chat-group-user__name">${user}</p>
+                  <p class="chat-group-user__name">"一致するユーザーはいません"</p>
                 </div>`
   }
 
-  $(".chat-group-user-form__input").on("keyup", function(){
-    var input = $(".chat-group-user-form__input").val();
-
+  userForm.on("keyup", function(){
+    var input = userForm.val();
+    // var inputs = input.split(" ").filter(function(e){ return e;});
+    // var newInputs = inputs.map(editElement);
+    // var word = newInputs.join("|");
+    // var reg = RegExp(word);
+    var data = { name: input}
+    //   keyword: word,
+    //   reg: reg,
+    //   preWord: preWord,
+    // console.log(name)
+    console.log(input)
+    // console.log(inputs)
+    // console.log(word)
+    // console.log(reg)
+    console.log(data)
     $.ajax({
-      type: 'get',
-      url: '/groups/new',
-      data: {keyword: input },
-      dataType: 'json'
+      type: 'GET',
+      url: '/users',
+      data: data,
+      dataType: 'json',
+      contentType: false
     })
 
     .done(function(users){
-      // $(".").empty();
+      searchResult.empty();
+      // appendUser(users);
+      // if (word != preWord) {
+      //     if(users.length !== 0) {
+      //       $.each(function(user){
+      //           appendUser(user);
+      //         })
+      //     }
+      //     else {
+      //       appendNoUser("一致するユーザーはいません")
+      //     }
+      //   preWord = word;
+      //   console.log(users)
+      // };
+            // $(".").empty();
       if(users.length !== 0) {
         users.forEach(function(user){
           appendUser(user);
@@ -33,6 +72,8 @@ $(function(){
       else {
         appendNoUser("一致するユーザーはいません")
       }
+      console.log(users)
     })
+
   });
 });
