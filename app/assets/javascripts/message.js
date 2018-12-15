25065a7eaa4e82ae3204898aef1cb79ba89c9e9c
Your Name <you@example.com>
2018-12-15 13:17:28 +0900

    Add auto reload function

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -19,6 +19,24 @@ $(function(){
                 </div>`
     return html;
   }
+  function buildMESSAGE(message) {
+    var html = `<div class="message" data-id='${message.id}'>
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                      ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                      ${message.date}
+                    </div>
+                  </div>
+                  <div class="lower-message">
+                      <p class="lower-message__content">
+                        ${message.content}
+                      </p>
+                  </div>
+                </div>`
+    $('.messages').append(html);
+  }
 
 
   $('#new_message').on('submit', function(e) {
@@ -49,6 +67,36 @@ $(function(){
     });
 
   })
+
+  $(function(){
+    setInterval(update,1000);
+  });
+
+    function update(){
+      var message_id = $('.message:last').data('id');
+      if (message_id == undefined){
+        $.noop;
+      }
+      var data = {
+        message: {id: message_id}
+      };
+      console.log(data);
+      $.ajax({
+        url: location.href,
+        type: "GET",
+        data: data,
+        dataType: 'json'
+      })
+      .done(function(data){
+        $.each(data, function(i,data){
+          buildMESSAGE(data);
+        $('.messages').animate({
+        scrollTop: $('.messages')[0].scrollHeight}, 1000, 'swing');
+        })
+        console.log(data)
+
+      })
+    }
 });
 
 
