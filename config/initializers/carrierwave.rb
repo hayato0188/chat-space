06c6764f5e7b9f7ae10c42035bea8eff26e23cd1
Your Name <you@example.com>
2018-12-17 17:19:07 +0900

    Fix

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -7,8 +7,7 @@ CarrierWave.configure do |config|
   config.fog_provider = 'fog/aws'
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id => ENV['S3_ACCESS_KEY'],
-    aws_secret_access_key => ENV['S3_SECRET_KEY'],
+    credentials: Aws::Credentials.new(ENV['AWS_ACCESS_KEY_ID'], ENV['AWS_SECRET_ACCESS_KEY'])
     region: 'ap-northeast-1'
   }
 
