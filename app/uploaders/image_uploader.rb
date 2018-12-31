ff08553539152deee175ca7a10bbb6290aace0e7
Your Name <you@example.com>
2018-12-31 09:59:12 +0900

    Add

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,6 +1,6 @@
 class ImageUploader < CarrierWave::Uploader::Base
   include CarrierWave::MiniMagick
-  process resize_to_fit: [800, 800]
+  process resize_to_fit: [300, 300]
 
   # Choose what kind of storage to use for this uploader:
   storage :fog
